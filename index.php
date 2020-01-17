b785db760fa4e5121c490e67bdb4691c1782c947
dirkstef <60003795+dirkstef@users.noreply.github.com>
2020-01-17 15:21:52 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,6 @@
+echo "# hr-walker" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/dirkstef/hr-walker.git
+git push -u origin master
